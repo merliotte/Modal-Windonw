83ef5e61b074e6d46d950016582495a41f064ee8
Merliotte <livainugent@hotmail.com>
2024-03-26 19:13:29 +0100

    fct close and open

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const bntShowModal = document.querySelectorAll('.show-modal');
+const bntCloseModal = document.querySelector('.close-modal');
+
+//Fonction Ouverture et fermeture
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// Créer une boucle pour appeler le texte des : showModal
+for (let i = 0; i < bntShowModal.length; i++)
+  bntShowModal[i].addEventListener('click', openModal);
+
+bntCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
