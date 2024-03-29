38f4a5b10dd1063d9d20beecae2ca0d5abd0e03b
Merliotte <livainugent@hotmail.com>
2024-03-29 17:49:04 +0100

    Fct Keydown "Escape"

--- script.js
+++ script.js
@@ -22,3 +22,9 @@ for (let i = 0; i < bntShowModal.length; i++)
 
 bntCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
